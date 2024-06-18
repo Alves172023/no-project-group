f1978803f383dd92b9da5675dd768d0ede2145e3
Alves172023 <145239833+Alves172023@users.noreply.github.com>
2024-06-17 20:54:31 -0400

    v1.0

---
+++ js/count.js
@@ -0,0 +1,23 @@
+
+//Valor Inicial - Altere se quiser
+const count = 34;
+
+
+const get_name = document.getElementById("");
+
+if(!get_name){
+    console.log("Erro ao iniciar o elemento!")
+    return;
+}
+
+function count_play(){
+    //Aqui vem a logica
+    const valor_init = get_name.innerText = count;
+    try{
+        cout++
+
+    }catch(err){
+        console.log(err)
+    }
+
+}
